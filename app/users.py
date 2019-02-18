0942725991f029f2cb83afdd35ecebb822a85557
Muart-C <charles.muathe@strathmore.edu>
2019-02-18 13:08:29 +0300

    [feature #164043405] add instance methods

--- app/users.py
+++ app/users.py
@@ -1,3 +1,4 @@
+import datetime
 """manage users"""
 class User():
     def __init__(self, name):
@@ -6,7 +7,23 @@ class User():
         self._last_seen = None
         self._class_received = self.__class__.__name__
 
-   
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+    def is_logged_in(self, is_logged_in):
+        return is_logged_in
+
+    def last_logged_in_at(self, _last_seen):
+        self._last_seen = _last_seen
+
+    def login(self, _online_status):
+        if _online_status is False:
+            self._online_status = True
+        return _online_status
+
 class moderator(User):
   def __init__(self, name):
     super().__init__(name)
