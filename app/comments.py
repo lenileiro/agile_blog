f096339300425b3dd2342fef0ff31090dc87bf52
Davidodari <davidkibzodari@gmail.com>
2019-02-18 12:47:31 +0300

    [Starts #164043638] Adds Delete Function in comments
    - Removves message from list
    - Adds gitignore to remove unneeded file .idea

--- app/comments.py
+++ app/comments.py
@@ -5,19 +5,27 @@ class Comment():
 
     def get_author(self):
         pass
-        
-    def set_message(self, message,author):
-        pass
 
-    def set_message(self, message,author):
+    def set_message(self, message, author):
         pass
 
     def created_at(self):
         pass
+
     def replied_to(self):
         pass
 
     def to_string(self):
         pass
 
-    
+    def delete_comment(self, message_id):
+        """Deletes comment from list"""
+        # Check instance of message id is int and list is not empty
+        if isinstance(message_id, int) and len(self.messages) > 0:
+            for message in self.messages:
+                # Check ids match
+                if message['id'] == message_id:
+                    self.messages.remove(message)
+                    return 'Comment Deleted'
+                return 'Comment Doesnt Exist'
+        return 'Invalid Id'
