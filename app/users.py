fb4cfca9da2fb86bc0bda4caa3a712f12a4e48d1
lenileiro <lenileiro@gmail.com>
2019-02-18 12:42:29 +0300

    [starts #164043405] added user constructor arguements

--- app/users.py
+++ app/users.py
@@ -1,4 +1,16 @@
 """manage users"""
 class User():
-    def __init__(self):
-        self.is_logged_in = False
+    def __init__(self, name):
+        self.name = name
+        self._online_status = False
+        self._last_seen = None
+        self._class_received = self.__class__.__name__
+
+   
+class moderator(User):
+  def __init__(self, name):
+    super().__init__(name)
+
+class admin(User):
+  def __init__(self, name):
+    super().__init__(name)
